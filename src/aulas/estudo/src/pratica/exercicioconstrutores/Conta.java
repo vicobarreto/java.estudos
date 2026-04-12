baf6a0d494087b76f35dcf01c83d5b0e56c44e1b
vicobarreto <vlimapaesbarreto@gmail.com>
2026-04-12 11:04:07 -0300

    classe conta

--- src/aulas/estudo/src/pratica/exercicioconstrutores/Conta.java
+++ src/aulas/estudo/src/pratica/exercicioconstrutores/Conta.java
@@ -1,5 +1,29 @@
 package pratica.exercicioconstrutores;
 
 public class Conta {
+ private int numeroconta;
+ private String nome;
+ private int conta;
 
+    public int getNumeroconta() {
+        return numeroconta;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
 }
---
+++ src/aulas/estudo/src/pratica/exercicioconstrutores/main.java
@@ -0,0 +1,7 @@
+package pratica.exercicioconstrutores;
+
+import java.util.Scanner;
+public class main {
+
+
+}
