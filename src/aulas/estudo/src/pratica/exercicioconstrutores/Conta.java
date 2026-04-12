b476308bd2a21b10592986f763acd67e340cea9b
vicobarreto <vlimapaesbarreto@gmail.com>
2026-04-12 12:14:22 -0300

    exercicio construtores feito!

--- src/aulas/estudo/src/pratica/exercicioconstrutores/Conta.java
+++ src/aulas/estudo/src/pratica/exercicioconstrutores/Conta.java
@@ -3,7 +3,23 @@ package pratica.exercicioconstrutores;
 public class Conta {
  private int numeroconta;
  private String nome;
- private int conta;
+ private double conta;
+private double deposito;
+private double saque;
+
+    public Conta(int numeroconta, String nome) {
+        this.numeroconta = numeroconta;
+        this.nome = nome;
+        this.conta = 0;
+    }
+
+    public Conta(int numeroconta, String nome, double conta) {
+        this.numeroconta = numeroconta;
+        this.nome = nome;
+        this.conta = conta;
+    }
+
+
 
     public int getNumeroconta() {
         return numeroconta;
@@ -19,11 +35,45 @@ public class Conta {
         this.nome = nome;
     }
 
-    public int getConta() {
+    public double getConta() {
         return conta;
     }
 
-    public void setConta(int conta) {
+    public void setConta(double conta) {
         this.conta = conta;
+    }
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }
+
+    public void depositoinicial( double minhaContadeposito){
+        conta = minhaContadeposito + conta;
+    }
+    public void depositar( double deposito) {
+conta = deposito + conta;
+    }
+    public void sacar( double saque ){
+        if(saque > 0 && saque <= this.conta) {
+            conta =  saque - conta;
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+
+            System.out.println("Erro: Saldo insuficiente ou valor inválido.");
+        }
+
+
     }
 }
--- src/aulas/estudo/src/pratica/exercicioconstrutores/main.java
+++ src/aulas/estudo/src/pratica/exercicioconstrutores/main.java
@@ -1,7 +1,64 @@
 package pratica.exercicioconstrutores;
 
 import java.util.Scanner;
+import java.util.Random;
+
 public class main {
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random gerador = new Random();
 
+        System.out.println("sistema bancario!");
+        System.out.println("criar conta! ");
+
+        int numero = gerador.nextInt(50); // 0 a 99
+        System.out.println("o numero da sua conta é: " + numero);
+
+        System.out.println("Digite o nome do conta: ");
+        String nome = input.nextLine();
+        Conta minhaConta = new Conta(numero, nome);
+
+
+        System.out.println("deseja adicionar saldo a sua conta?");
+        Boolean sim = input.nextBoolean();
+        if (sim) {
+            System.out.println("digite o valor do deposito: ");
+            double minhaContadeposito = input.nextDouble();
+            minhaConta.depositoinicial(minhaContadeposito);
+        }
+
+
+        System.out.println("conta criada com sucesso!");
+
+        System.out.println("deseja movimentar sua conta?");
+        Boolean movimentar = input.nextBoolean();
+
+        while (movimentar) {
+            System.out.println("1 - depositar . 2 - sacar ");
+            int opcao = input.nextInt();
+            if (opcao == 1) {
+                System.out.println("deseja depositar quanto na conta? ");
+                double deposito = input.nextDouble();
+                minhaConta.depositar(deposito);
+            }
+            if (opcao == 2) {
+                System.out.println("deseja sacar quanto na conta? ");
+                double saque = input.nextDouble();
+                if(minhaConta.getConta() < 0){
+                    System.out.println("saldo insuficiente");
+                }
+                minhaConta.sacar(saque);
+            }
+            System.out.println("saldo atual: " + minhaConta.getConta());
+
+            System.out.println("deseja continuar operação?");
+            Boolean movimentar1 = input.nextBoolean();
+            movimentar = movimentar1;
+
+        }
+
+
+    }
 }
+
