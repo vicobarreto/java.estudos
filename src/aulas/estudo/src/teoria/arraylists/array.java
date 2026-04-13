6262d5116bf95904cc1134f5836ae6f2ffb464a0
vicobarreto <vlimapaesbarreto@gmail.com>
2026-04-13 11:13:13 -0300

    comeco de array

--- src/aulas/estudo/src/teoria/arraylists/array.java
+++ src/aulas/estudo/src/teoria/arraylists/array.java
@@ -1,9 +1,18 @@
 package teoria.arraylists;
 
+import java.util.Scanner;
+
 public class array {
+    /*
+    tamanho fixo
+     */
    public static void main(String[] args) {
-        int p;
-        p = 10;
-        System.out.println(p);
-    }
+
+       Scanner input = new Scanner(System.in);
+   int n = input.nextInt();
+   double[] vect = new double[n];
+   for( int i = 0 ; i < n; i++){
+
+   }
+   }
 }
